09912854e5f815466661a6eacb3fbf76b051f70d
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-10-22 23:34:33 +0200

    Render overview page
    Finally fixed the thing, and using this replace template stuff.
    Don't know if in practice people do the same thing {%SOME_TEMPLATE%}

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -37,6 +37,7 @@ const url = require("url");
 // incoming request, sending reponse
 
 // this code runs only once,
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -50,6 +51,21 @@ const tempProduct = fs.readFileSync(
   "utf-8"
 );
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
@@ -61,7 +77,12 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    res.end(tempOverview);
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      ;
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    console.log(cardsHtml);
+    res.end(output);
 
     // Product page
   } else if (pathName === "/product") {
