6e6710d494a2fba6a98fcd3d4b4a10539ad805b9
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-02 20:59:36 +0100

    some changes

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      console.log(res.body.message);
+
+      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+        console.log("Random dog image saved to file.");
+      });
+    });
+});
