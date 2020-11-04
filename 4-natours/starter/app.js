7a90b81115cccdfc5f2e710ad5a60e11900184ed
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-04 22:38:57 +0100

    Get method

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,16 +3,6 @@ const express = require('express');
 // express is a function and with that function calling, app has buntch of methods calling.
 const app = express();
 
-// // get method, on url '/', get the res
-// app.get('/', (req, res) => {
-//   // we can use 200 or OK
-//   res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -21,6 +11,7 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       tours,
     },
