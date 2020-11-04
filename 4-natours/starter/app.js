b4b531130b0e0a9c4720e723c9fa05506786c1f7
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-04 22:58:44 +0100

    Post method

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,11 @@ const express = require('express');
 // express is a function and with that function calling, app has buntch of methods calling.
 const app = express();
 
+app.use(express.json());
+// express.json() is middleware which is a function that can modify the incoming request data.
+// It's called middleware because it stands between.
+// in the middle of request and response
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -16,6 +21,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  // res.send('done');
 });
 
 // begin a server
