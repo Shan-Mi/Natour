3de9589e04d0dd4f9ad26fc5942c2d6585dd794a
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-10 21:39:01 +0100

    Added checkid middleware

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -3,6 +3,19 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+  const id = Number(req.params.id);
+  const tour = tours.find((tour) => tour.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -18,20 +31,13 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = Number(req.params.id);
   const tour = tours.find((tour) => tour.id === id);
-  if (tour) {
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tour,
-      },
-    });
-  } else {
-    res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
 };
 
 exports.postTour = (req, res) => {
@@ -56,13 +62,6 @@ exports.postTour = (req, res) => {
 
 exports.updateTour = (req, res) => {
   const id = Number(req.params.id);
-  const tour = tours.find((tour) => tour.id === id);
-  if (!tour) {
-    res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -73,13 +72,6 @@ exports.updateTour = (req, res) => {
 
 exports.deleteTour = (req, res) => {
   const id = Number(req.params.id);
-  const tour = tours.find((tour) => tour.id === id);
-  if (!tour) {
-    res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null,
