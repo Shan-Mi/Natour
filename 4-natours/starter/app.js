60727ead49dd202f91ab7d7760b6970cc754281d
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-14 00:02:14 +0100

    Preparation before installing mongodb

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,7 +8,11 @@ const app = express();
 
 // 1) middlewares
 // 'tiny' is another params in morgan
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' }); // this config has to become before require app
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+// console.log(process.env);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
