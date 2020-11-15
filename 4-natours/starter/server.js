33ed7dd6fe84024ef8c23b575aba45fca63aeccd
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-15 22:54:52 +0100

    Test adding docs into collection

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -39,6 +39,21 @@ const tourSchema = new mongoose.Schema({
 // create a model
 const Tour = mongoose.model('Tour', tourSchema);
 
+// testTour is an instance of Tour model, with couple of methods on it
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save() // save it in the database
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 👻👺: ', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
