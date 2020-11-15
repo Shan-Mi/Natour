fb5a3e40b23ad7b8ad35e88bfb34424bafb836be
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-15 20:55:03 +0100

    With cl connect

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' }); // this config has to become before require app
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// options to handle with deprecation warnings
+// this connect will return a promise
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
