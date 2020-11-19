711233a5ccf4e2bea7ae57186d039142dbc9ed75
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-19 23:42:37 +0100

    Implement filtering in query

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -6,14 +6,23 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // 1. BUILD QUERY
+    // 1) filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    console.log(req.query, queryObj);
+    // console.log(req.query, queryObj);
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    // { duration: { gte: '5' }, difficulty: 'easy' }
+    // { difficuty: 'easy', duration: {$gte: 5}}
+    // gte, gt, lte, lt
 
+    const query = Tour.find(JSON.parse(queryStr));
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
