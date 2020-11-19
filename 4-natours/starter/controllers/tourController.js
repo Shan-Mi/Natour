f2e20cd5b41530b0f0792f693c8df33109707d61
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-19 22:14:55 +0100

    Query's 3 steps

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -6,19 +6,24 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // 1. BUILD QUERY
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
     console.log(req.query, queryObj);
 
-    const tours = await Tour.find(queryObj);
+    const query = Tour.find(queryObj);
 
-    // const tours = await Tour.find()
+    // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
 
+    // 2. EXECUTE QUERY
+    const tours = await query;
+
+    // 3. SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
