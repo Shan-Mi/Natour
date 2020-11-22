c43a85547f2dd619670875284b0341150de76409
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-22 22:09:26 +0100

    Added error router

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -17,11 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('🌺 Hello from the middleware❗');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -29,6 +24,13 @@ app.use((req, res, next) => {
 // mount routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
 
