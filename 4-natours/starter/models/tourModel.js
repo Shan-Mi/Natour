14eef25698acd038f3e7706c1d756b5cb6419d9b
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-22 16:34:56 +0100

    Validator and custom validators

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -54,7 +55,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+          // this only points to current doc on new document creation
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price.',
+      },
+    },
     summary: {
       type: String,
       trim: true,
