f0d086069db557c20b32576461cfea6398a89dab
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-28 21:08:04 +0100

    Sending different errors in dev and prod

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,10 +1,29 @@
-module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-  // because errors come from mongoose, so there is no statusCode
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+  // because errors come from mongoose, so there is no statusCode
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
