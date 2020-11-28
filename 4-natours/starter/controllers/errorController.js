dc7985090e2164e8e2b8d6ced1a285b00fce9c37
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-28 22:27:10 +0100

    Mongoose validation errors

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -11,12 +11,20 @@ const handleDuplicateFieldsDB = (err) => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('| ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
     stack: err.stack,
+    name: err.name,
   });
 };
 
@@ -60,6 +68,9 @@ module.exports = (err, req, res, next) => {
       error = handleDuplicateFieldsDB(error);
     }
 
+    if (error.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
     sendErrorProd(error, res);
   }
 };
