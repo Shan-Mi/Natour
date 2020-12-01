4cd26d2da1834a5f4fc65bbc58def29010a05b02
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-12-01 22:23:02 +0100

    JWT for signup user

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,14 +1,25 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  // now we cannot register as an admin
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  // payload: id: newUser._id
+  // header is created automatically
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
-
