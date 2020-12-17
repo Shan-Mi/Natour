ea093273abca62b812710995bacc255ebce73427
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-12-17 21:23:58 +0100

    :heavy_plus_sign: hpp & whitelist fields

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -43,6 +44,21 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS (html code)
 app.use(xss());
+
+// prevent parameter polotion, accept the last one.
+// for these fields on whitelist, they will not be affected
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
