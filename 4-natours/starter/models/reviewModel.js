d10f9f362e0304da4f4a0871c7f0acb172db83d8
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-12-21 16:24:47 +0100

    Statics method on schema
    Now when user creates a new review, rating will be calculated and updated on this tour's review.

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -1,5 +1,6 @@
 // review / rating / createdAt / ref to tour / ref to user
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -38,6 +39,40 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+// static method on schema
+// aggregation pipline is needed. aggregate method f.ex.
+// this point to current model, so aggregate on the model
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  console.log(tourId);
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour', // group by tour
+        nRating: { $sum: 1 }, // add amount, begin by 1
+        avgRating: { $avg: '$rating' }, //each field has a rating
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+// pre save, it will not be in the collection yet
+// post, all documents are stored
+// save cannnot access to next
+reviewSchema.post('save', function () {
+  // this points to current review
+  // constructor is the model which create this document
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
