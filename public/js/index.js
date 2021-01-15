4603c11812f749ae8b113b1552b89e9c433c4016
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-15 15:28:34 +0100

    Send stripe payment
    1. Create function to redirect to checkout for stripe;
    2. Update checkout button text;
    3. Use axios to post payload to stripe.

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,16 @@
-import '@babel/polyfill';
-import { displayMap } from './mapbox';
-import { login, logout } from './login';
-import { updateSettings } from './updateSettings';
+import "@babel/polyfill";
+import { displayMap } from "./mapbox";
+import { login, logout } from "./login";
+import { updateSettings } from "./updateSettings";
+import { bookTour } from "./stripe";
 
 // DOM elements
-const mapBox = document.querySelector('#map');
-const loginForm = document.querySelector('#user-login-form');
-const logoutBtn = document.querySelector('.nav__el--logout');
-const userDataForm = document.querySelector('.form-user-data');
-const userSettingForm = document.querySelector('.form-user-settings');
+const mapBox = document.querySelector("#map");
+const loginForm = document.querySelector("#user-login-form");
+const logoutBtn = document.querySelector(".nav__el--logout");
+const userDataForm = document.querySelector(".form-user-data");
+const userSettingForm = document.querySelector(".form-user-settings");
+const bookBtn = document.querySelector("#book-tour");
 // const userPhoto = document.que
 
 // delegation
@@ -19,37 +21,37 @@ if (mapBox) {
 }
 
 if (loginForm) {
-  loginForm.addEventListener('submit', (e) => {
+  loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
     login(email, password);
   });
 }
 
 if (logoutBtn) {
-  logoutBtn.addEventListener('click', () => {
+  logoutBtn.addEventListener("click", () => {
     logout();
   });
 }
 
 if (userDataForm) {
-  userDataForm.addEventListener('submit', async (e) => {
+  userDataForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const form = new FormData();
     // The FormData() constructor creates a new FormData object.
-    const name = document.querySelector('#name').value.trim();
-    const email = document.querySelector('#email').value.trim();
-    const [photo] = document.querySelector('#photo').files;
+    const name = document.querySelector("#name").value.trim();
+    const email = document.querySelector("#email").value.trim();
+    const [photo] = document.querySelector("#photo").files;
     // files is an array, and we want the first one
 
-    form.append('name', name);
-    form.append('email', email);
-    form.append('photo', photo);
+    form.append("name", name);
+    form.append("email", email);
+    form.append("photo", photo);
 
-    await updateSettings(form, 'data');
-    document.querySelector('#name').value = name;
-    document.querySelector('#email').value = email;
+    await updateSettings(form, "data");
+    document.querySelector("#name").value = name;
+    document.querySelector("#email").value = email;
 
     setTimeout(() => {
       location.reload();
@@ -58,19 +60,27 @@ if (userDataForm) {
 }
 
 if (userSettingForm) {
-  userSettingForm.addEventListener('submit', async (e) => {
+  userSettingForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    document.querySelector('.btn--save-password').textContent = 'Updating...';
-    const passwordCurrent = document.querySelector('#password-current').value;
-    const password = document.querySelector('#password').value;
-    const passwordConfirm = document.querySelector('#password-confirm').value;
+    document.querySelector(".btn--save-password").textContent = "Updating...";
+    const passwordCurrent = document.querySelector("#password-current").value;
+    const password = document.querySelector("#password").value;
+    const passwordConfirm = document.querySelector("#password-confirm").value;
     await updateSettings(
       { passwordCurrent, password, passwordConfirm },
-      'password'
+      "password"
     );
-    document.querySelector('.btn--save-password').textContent = 'Save password';
-    document.querySelector('#password-current').value = '';
-    document.querySelector('#password').value = '';
-    document.querySelector('#password-confirm').value = '';
+    document.querySelector(".btn--save-password").textContent = "Save password";
+    document.querySelector("#password-current").value = "";
+    document.querySelector("#password").value = "";
+    document.querySelector("#password-confirm").value = "";
+  });
+}
+
+if (bookBtn) {
+  bookBtn.addEventListener("click", (e) => {
+    e.target.textContent = "Processing...";
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
 }
---
+++ public/js/stripe.js
@@ -0,0 +1,21 @@
+const stripe = Stripe(
+  "pk_test_51I9dJhAJ3ueSRZl46pODU6F8qZmKuNku3TbljUrm5MK7cewQ9QC7vefd8wUKLe5UN7MCIZbq6C6DOSzF0mUJgbVt00uJ0AVPN6"
+);
+import { showAlert } from "./alerts";
+import axios from "axios";
+const rootURL = "http://localhost:8000";
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from endpoint
+    const session = await axios.post(
+      `/api/v1/bookings/checkout-session/${tourId}`
+    );
+
+    await stripe.redirectToCheckout({ sessionId: session.data.session.id });
+
+    // 2) Create checkout form + charge credit card
+  } catch (err) {
+    showAlert("error", err);
+  }
+};
